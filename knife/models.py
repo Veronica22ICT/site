0e380440b214cbd903c9d0877936b230f2c0c3ff
Veronica22ICT <matvijchuk09072008@gmail.com>
2025-04-01 13:53:05 +0300

    my_project

--- knife/models.py
+++ knife/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 
-
 class Knife(models.Model):
     name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='knives/', null=False, default='path/to/default_image.jpg')
+    image = models.ImageField(upload_to='knives/', null=False, default='path/to/default_image')
     description = models.TextField()
     types = models.CharField(max_length=100)
     mechanism = models.CharField(max_length=100)
     price = models.IntegerField()
     available = models.IntegerField()
+    post_office = models.CharField(max_length=50, null=True, blank=True)
     time = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return self.name
@@ -21,6 +22,7 @@ class Care (models.Model):
     description = models.TextField()
     price = models.IntegerField()
     available = models.IntegerField()
+    post_office = models.CharField(max_length=50, null=True, blank=True)
     time = models.DateTimeField(auto_now_add=True)
 
 
@@ -30,4 +32,5 @@ class Repair (models.Model):
     description = models.TextField()
     price = models.IntegerField()
     available = models.IntegerField()
+    post_office = models.CharField(max_length=50, null=True, blank=True)
     time = models.DateTimeField(auto_now_add=True)
